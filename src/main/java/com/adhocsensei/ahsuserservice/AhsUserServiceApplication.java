928d0cb4f0e17506479516daa0da68e6530cbe57
Cody Wyoming <68657852+npokamestov@users.noreply.github.com>
2021-06-24 23:28:23 -0400

    transfered from sensei api

---
+++ src/main/java/com/adhocsensei/ahsuserservice/AhsUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.adhocsensei.ahsuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhsUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AhsUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adhocsensei/ahsuserservice/AhsUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adhocsensei.ahsuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhsUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
